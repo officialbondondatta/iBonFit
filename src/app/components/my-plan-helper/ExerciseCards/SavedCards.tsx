f1772dc7745063ca46c1cb72834ba579f19cc1c3
officialbondondatta <dattabondon1320@gmail.com>
2026-09-25 14:58:24 +0600

    added remove and mark as done functionalities to cards

---
+++ src/app/components/my-plan-helper/ExerciseCards/SavedCards.tsx
@@ -0,0 +1,56 @@
+import { IExercise } from "@/app/types/types";
+import Image from "next/image";
+import Link from "next/link";
+import { useContext } from "react";
+import { CiStar } from "react-icons/ci";
+import { LuClock4 } from "react-icons/lu";
+import { RxCross1 } from "react-icons/rx";
+import { VscFlame } from "react-icons/vsc";
+import { GymContext } from "../../GymContext/GymContext";
+import { toast } from "react-toastify";
+
+interface IPlanCardProps {
+    currentPlan: IExercise
+}
+
+const SavedCards = ({ currentPlan }: IPlanCardProps) => {
+    const { saved, setSaved } = useContext(GymContext)
+
+    const handleRemove = () => {
+        const newPlans = saved.filter((p) => p.id !== currentPlan.id)
+        setSaved(newPlans)
+        toast.success("Removed from saved")
+    }
+    return (
+        <div key={currentPlan.id} className=" bg-slate-600 rounded-xl grid lg:grid-cols-2 grid-cols-1 lg:items-center lg:justify-center lg:gap-5 gap-2">
+            <div className="flex flex-col lg:flex-row lg:items-center">
+                <div className="lg:p-5 py-2 place-self-center">
+                    <Image src={currentPlan.image} alt={`${currentPlan?.name} image`} width={200} height={50} className="max-h-30 lg:p-0 p-2 lg:w-50 w-90 object-cover rounded-2xl"></Image>
+                </div>
+                <div className="px-5">
+                    <h2 className="text-xl font-semibold">{currentPlan.name}</h2>
+                    <span className="text-sm">{currentPlan.equipment}</span>
+                    <ul className="flex lg:gap-5 gap-2 items-center mt-2 text-sm">
+                        <li className="flex items-center gap-2 text-slate-200 font-semibold"><LuClock4 className="text-lime-400" />{currentPlan.duration} min</li>
+                        <li className="flex items-center gap-2 text-slate-200 font-semibold"><VscFlame className="text-lime-400" />{currentPlan.caloriesBurned} kcal</li>
+                        <li className="flex items-center gap-2 text-slate-200 font-semibold"><CiStar className="text-lime-400 font-bold text-lg" />{currentPlan.rating}</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="flex gap-5  justify-self-start lg:justify-self-end lg:pr-5 p-5">
+                <div>
+                    <Link href={`/exercise/${currentPlan.id}`} className="flex  justify-center cursor-pointer  items-center gap-2 border border-white text-white font-semibold lg:px-4 px-2 py-2 rounded-2xl">
+                        <span className="text-xs">
+                            View Details
+                        </span>
+                    </Link>
+                </div>
+                <div className="flex items-center justify-center font-bold text-lg">
+                    <button onClick={handleRemove}><RxCross1 className="cursor-pointer" /></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SavedCards;
--- src/app/my-plan/page.tsx
+++ src/app/my-plan/page.tsx
@@ -4,14 +4,8 @@ import SectionHeader from "../components/shared/SectionHeader";
 import { GymContext } from "../components/GymContext/GymContext";
 import ExerciseStats from "../components/my-plan-helper/ExerciseStats";
 import NoDataYet from "../components/my-plan-helper/NoDataYet";
-import Image from "next/image";
-import { LuClock4 } from "react-icons/lu";
-import { VscFlame } from "react-icons/vsc";
-import { IoMdFlame } from "react-icons/io";
-import { CiStar } from "react-icons/ci";
-import Link from "next/link";
-import { MdOutlineAddCard, MdOutlineDone } from "react-icons/md";
-import { RxCross1 } from "react-icons/rx";
+import PlanCards from "../components/my-plan-helper/ExerciseCards/PlanCards";
+import SavedCards from "../components/my-plan-helper/ExerciseCards/SavedCards";
 
 const MyPlanPage = () => {
     const sectionDetails = {
@@ -79,49 +73,12 @@ const MyPlanPage = () => {
                     {
                         selectedTab === "plan" ?
                             sortedPlanData.length === 0 ? <NoDataYet></NoDataYet> : sortedPlanData.map((plan) => (
-                                <div key={plan.id} className=" bg-slate-600 rounded-xl grid lg:grid-cols-2 grid-cols-1 lg:items-center lg:justify-center lg:gap-5 gap-2">
-                                    <div className="flex flex-col lg:flex-row lg:items-center">
-                                        <div className="lg:p-5 py-2 place-self-center">
-                                            <Image src={plan.image} alt={`${plan?.name} image`} width={200} height={50} className="max-h-30 lg:p-0 p-2 lg:w-50 w-90 object-cover rounded-2xl"></Image>
-                                        </div>
-                                        <div className="px-5">
-                                            <h2 className="text-xl font-semibold">{plan.name}</h2>
-                                            <span className="text-sm">{plan.equipment}</span>
-                                            <ul className="flex lg:gap-5 gap-2 items-center mt-2 text-sm">
-                                                <li className="flex items-center gap-2 text-slate-200 font-semibold"><LuClock4 className="text-lime-400" />{plan.duration} min</li>
-                                                <li className="flex items-center gap-2 text-slate-200 font-semibold"><VscFlame className="text-lime-400" />{plan.caloriesBurned} kcal</li>
-                                                <li className="flex items-center gap-2 text-slate-200 font-semibold"><CiStar className="text-lime-400 font-bold text-lg" />{plan.rating}</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="flex gap-5  justify-self-start lg:justify-self-end lg:pr-5 p-5">
-                                        <div>
-                                            <Link href={`/exercise/${plan.id}`} className="flex  justify-center cursor-pointer  items-center gap-2 border border-white text-white font-semibold lg:px-4 px-2 py-2 rounded-2xl">
-                                                <span className="text-xs">
-                                                    View Details
-                                                </span>
-                                            </Link>
-                                        </div>
-                                        <div>
-                                            <Link href={"/"} className="flex  justify-center cursor-pointer  items-center gap-2 bg-lime-300 text-slate-800 font-semibold lg:px-4 px-2 py-2 rounded-2xl">
-                                                <MdOutlineDone className="text-xl" />
-                                                <span className="text-xs">
-                                                    Mark as Done
-                                                </span>
-                                            </Link>
-                                        </div>
-                                        <div className="flex items-center justify-center font-bold text-lg">
-                                            <button><RxCross1 /></button>
-                                        </div>
-                                    </div>
-                                </div>
+                                <PlanCards key={plan.id} currentPlan={plan}></PlanCards>
                             ))
                             :
                             sortedSavedData.length === 0 ? <NoDataYet></NoDataYet> : sortedSavedData.map((saved) => (
-                                <div key={saved.id}>
-                                    <h1>{saved.name}</h1>
-                                    <span>{saved.duration}</span>
-                                </div>
+                                <SavedCards key={saved.id} currentPlan={saved}></SavedCards>
+
                             ))
                     }
                 </div>
