6a54ae74d6d2719e7903b192f0ef5ce55a533b8d
officialbondondatta <dattabondon1320@gmail.com>
2026-09-25 09:26:24 +0600

    added footer

---
+++ src/app/components/shared/Footer.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/assets/logo.png"
+const Footer = () => {
+    return (
+        <footer className="bg-slate-700 p-5 mt-15">
+            <div className="max-w-300 mx-auto flex items-center space-y-3 lg:flex-row flex-col justify-center lg:justify-between">
+                <div className="flex items-center gap-2">
+                    <Image src={Logo} alt="ibonfit logo" className="h-5 w-5"></Image>
+                    <Link href={"/"} className=" font-bold text-lg">
+                        iBonFit
+                    </Link>
+                </div>
+                <div>
+                    <h2 className="text-slate-200 text-sm text-center">© 2026 FitLog — Workout Library. Train hard, log honest.</h2>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/shared/Navbar";
 import GymProvider from "./components/GymContext/GymContext";
 import { ToastContainer } from "react-toastify";
+import Footer from "./components/shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <Navbar>
           </Navbar>
           {children}
+          <Footer></Footer>
         </GymProvider>
         <ToastContainer />
       </body>
