6752d4d9699f5f5bfe5370f90cbf88e8b63e135d
officialbondondatta <dattabondon1320@gmail.com>
2026-09-26 10:16:49 +0600

    fixed some responsive issues

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 import BannerImg from "@/assets/banner.png"
 const Banner = () => {
     return (
-        <section className="py-5 lg:py-0">
-            <div className="bg-linear-to-r grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center justify-center lg:gap-2 gap-10 from-lime-200 via-lime-100 to-indigo-100 my-5 p-10 rounded-2xl" >
-                <div className="flex flex-col lg:text-left text-center pl-5 justify-center text-slate-500  space-y-4">
-                    <span className="text-sm self-center lg:self-start border-2 border-white bg-lime-300 text-slate-600 font-semibold px-2 py-1 rounded-full">WORKOUT LIBRARY</span>
-                    <h2 className="lg:text-4xl text-2xl font-bold text-slate-700 max-w-90">TRAIN WITH INTENT. LOG
+        <section className="py-5 lg:py-0 ">
+            <div className="bg-linear-to-r w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center md:justify-self-start justify-center lg:gap-2 gap-10 bg-slate-800 my-5 p-10 rounded-2xl" >
+                <div className="flex flex-col lg:text-left md:text-left text-center pl-5 justify-center text-slate-500  space-y-4">
+                    <span className="text-sm self-center md:self-start lg:self-start  text-lime-300 font-semibold">WORKOUT LIBRARY</span>
+                    <h2 className="lg:text-4xl text-2xl font-bold text-white max-w-90">TRAIN WITH INTENT. LOG
                         EVERY SET.</h2>
-                    <p className="text-slate-700">FitLog is a dark, no-nonsense gym companion: pick a lift, lock it <br />
+                    <p className="text-white">FitLog is a dark, no-nonsense gym companion: pick a lift, lock it <br />
                         into today&apos;s plan, and watch the week&apos;s work add up.</p>
-                    <a href="#library" className="lg:self-start self-center  bg-lime-300 text-slate-900 font-semibold px-3 py-2 border border-white rounded-md cursor-pointer">
+                    <a href="#library" className="lg:self-start md:self-start self-center  bg-lime-300 text-slate-900 font-semibold px-3 py-2 border border-white rounded-md cursor-pointer">
                         BROWSE WORKOUTS
                     </a>
                 </div>
@@ -19,7 +19,7 @@ const Banner = () => {
                 </div>
 
             </div>
-        </section>
+        </section >
     );
 };
 export default Banner;
