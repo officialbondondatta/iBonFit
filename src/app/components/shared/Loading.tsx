14a89b2a9b1bd9448755ca9038596fc6c67eb290
officialbondondatta <dattabondon1320@gmail.com>
2026-09-26 08:46:29 +0600

    fixed rezponsive issue of details page

--- src/app/components/shared/Loading.tsx
+++ src/app/components/shared/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
     return (
-        <div className="container mx-auto flex items-center justify-center mt-10">
+        <div className="container mx-auto flex items-center justify-center mt-10 min-h-screen">
             <span className="loading loading-spinner w-15 text-xl text-center"></span>
         </div>
     );
